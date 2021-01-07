0d82704aa4fa5af9e316d6c21b66bb6c01c02702
Onur Bilgin <onurbil@hotmail.com>
2021-01-07 11:00:46 +0100

    cleaning

--- train.py
+++ train.py
@@ -1,9 +1,8 @@
 import torch
 from torch import nn, optim
 import torch.nn.functional as F
-from torch.utils.tensorboard import SummaryWriter
 import time
-from utils import data_loader_wind, data_loader_wind_nl, data_loader_wind_us
+from utils import data_loader_wind_us
 from models import wind_models
 from tqdm import tqdm
 import scipy.io as sio
@@ -68,7 +67,7 @@ def loss_batch_scaled(model, loss_func, xb, yb, opt=None, scaler=None):
 
 
 def test_model(data_folder, model, loss_func, dev="cpu", scaler=None):
-    test_dl = data_loader_wind.get_test_loader(data_folder,
+    test_dl = data_loader_wind_us.get_test_loader(data_folder,
                                                batch_size=64,
                                                num_workers=4,
                                                pin_memory=True if dev == torch.device("cuda") else False)
@@ -87,83 +86,6 @@ def test_model(data_folder, model, loss_func, dev="cpu", scaler=None):
         test_loss /= test_num
 
     print(f"Test loss: {test_loss}")
-
-
-def fit(epochs, model, loss_func, opt, train_dl, valid_dl,
-        device=torch.device('cpu'), save_every: int = None, tensorboard: bool = False, earlystopping=None):
-    if tensorboard:
-        writer = SummaryWriter(comment=f"_wind_NL_1h_{model.__class__.__name__}")
-    start_time = time.time()
-    best_val_loss = 1e300
-    earlystopping_counter = 0
-    for epoch in tqdm(range(epochs), desc="Epochs"):
-        model.train()
-        train_loss = 0.0
-        total_num = 0
-        # for i, (xb, yb) in enumerate(tqdm(train_dl, desc="Batches", leave=False)):
-        for i, (xb, yb) in enumerate(train_dl):
-            loss, num = loss_batch(model, loss_func, xb.to(device), yb.to(device), opt)
-            train_loss += loss
-            total_num += num
-            # if i > 100:
-            #     break
-        train_loss /= total_num
-
-        # Calc validation loss
-        val_loss = 0.0
-        val_num = 0
-        model.eval()
-        with torch.no_grad():
-            # for xb, yb in tqdm(valid_dl, desc="Validation", leave=False):
-            for xb, yb in valid_dl:
-                loss, num = loss_batch(model, loss_func, xb.to(device), yb.to(device))
-                val_loss += loss
-                val_num += num
-            val_loss /= val_num
-        #     losses, nums = zip(
-        #         *[loss_batch(model, loss_func, xb.to(device), yb.to(device)) for xb, yb in valid_dl]
-        #     )
-        # val_loss = np.sum(np.multiply(losses, nums)) / np.sum(nums)
-
-        # Save the model with the best validation loss
-        if val_loss < best_val_loss:
-            torch.save({
-                'model': model,
-                'epoch': epoch,
-                'state_dict': model.state_dict(),
-                'optimizer_state_dict': opt.state_dict(),
-                'val_loss': val_loss,
-                'train_loss': train_loss
-            }, f"models/checkpoints/best_val_loss_model_{model.__class__.__name__}.pt")
-            best_val_loss = val_loss
-            earlystopping_counter = 0
-
-        else:
-            if earlystopping is not None:
-                earlystopping_counter += 1
-                if earlystopping_counter >= earlystopping:
-                    print(f"Stopping early --> val_loss has not decreased over {earlystopping} epochs")
-                    break
-
-        print(f"Epoch: {epoch:5d}, Time: {(time.time() - start_time) / 60:.3f} min, "
-              f"Train_loss: {train_loss:2.10f}, Val_loss: {val_loss:2.10f}"
-              f", Early stopping counter: {earlystopping_counter}/{earlystopping}" if earlystopping is not None else "")
-
-        if tensorboard:
-            # add to tensorboard
-            writer.add_scalar('Loss/train', train_loss, epoch)
-            writer.add_scalar('Loss/val', val_loss, epoch)
-        if save_every is not None:
-            if epoch % save_every == 0:
-                # save model
-                torch.save({
-                    'model': model,
-                    'epoch': epoch,
-                    'state_dict': model.state_dict(),
-                    'optimizer_state_dict': opt.state_dict(),
-                    'val_loss': val_loss,
-                    'train_loss': train_loss
-                }, f"models/checkpoints/model_epoch_{epoch}.pt")
 
 
 def train_wind_us(data_folder, epochs, input_timesteps, prediction_timestep, num_cities, num_features, city_idx=None,
@@ -206,12 +128,11 @@ def train_wind_us(data_folder, epochs, input_timesteps, prediction_timestep, num
     # loss_func = F.mse_loss
     loss_func = F.l1_loss
     #### Training ####
-    save_every = 10
-
     best_val_loss = 1e300
 
     earlystopping_counter = 0
-    for epoch in tqdm(range(epochs), desc="Epochs"):
+    pbar = tqdm(range(epochs), desc="Epochs")
+    for epoch in pbar:
         model.train()
         train_loss = 0.0
         total_num = 0
@@ -233,20 +154,12 @@ def train_wind_us(data_folder, epochs, input_timesteps, prediction_timestep, num
                 val_loss += loss
                 val_num += num
             val_loss /= val_num
-
+        pbar.set_postfix({'train_loss': train_loss, 'val_loss': val_loss})
         # Save the model with the best validation loss
         if val_loss < best_val_loss:
-            torch.save({
-                'model': model,
-                'epoch': epoch,
-                'state_dict': model.state_dict(),
-                'optimizer_state_dict': opt.state_dict(),
-                'val_loss': val_loss,
-                'train_loss': train_loss
-            }, f"models/checkpoints/best_val_loss_model_{model.__class__.__name__}.pt")
             best_val_loss = val_loss
             earlystopping_counter = 0
-
+        
         else:
             if earlystopping is not None:
                 earlystopping_counter += 1
@@ -254,34 +167,14 @@ def train_wind_us(data_folder, epochs, input_timesteps, prediction_timestep, num
                     print(f"Stopping early --> val_loss has not decreased over {earlystopping} epochs")
                     break
 
-        if save_every is not None:
-            if epoch % save_every == 0:
-                # save model
-                torch.save({
-                    'model': model,
-                    'epoch': epoch,
-                    'state_dict': model.state_dict(),
-                    'optimizer_state_dict': opt.state_dict(),
-                    'val_loss': val_loss,
-                    'train_loss': train_loss
-                }, f"models/checkpoints/model_epoch_{epoch}.pt")
-
-    ### Finished training ###
-    # Save best model
-    load_best_val_model = torch.load(f"models/checkpoints/best_val_loss_model_{model.__class__.__name__}.pt")
-    torch.save({'model': load_best_val_model['model'],
-                'state_dict': load_best_val_model['state_dict']},
-               f"models/trained_models/wind_model_NL_{prediction_timestep}h_{model.__class__.__name__}.pt")
-
 
 if __name__ == "__main__":
     dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     # torch.backends.cudnn.benchmark = True
-    folder = "data/"
 
-    print("NL dataset. Step: ", 4)
-    data = "Wind_data_US/dataset_tensor.npy"
-    train_wind_us(folder+data, num_cities=29, num_features=11, city_idx=4, epochs=150, input_timesteps=6,
+    print("Weather dataset. Step: ", 4)
+    data = "../processed_dataset/dataset_tensor.npy"
+    train_wind_us(data, num_cities=29, num_features=11, city_idx=4, epochs=1, input_timesteps=6,
                   prediction_timestep=4, dev=dev, earlystopping=20)
 
 
